90c720e410df240a60eef241452be861e26fb570
Vasily Maslov <44157132+Stubbs221@users.noreply.github.com>
2022-09-18 17:54:41 +0300

    Initial commit

---
+++ LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Vasily Maslov on 18.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
