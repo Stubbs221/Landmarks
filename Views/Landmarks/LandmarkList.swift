bd27f3479ad973382fe22aaf901748aa3c91b6f9
Vasily Maslov <44157132+Stubbs221@users.noreply.github.com>
2022-09-19 17:38:18 +0300

    Added data managing and handling user input

---
+++ Views/Landmarks/LandmarkList.swift
@@ -0,0 +1,48 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Vasily Maslov on 18.09.2022.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    @EnvironmentObject var modelData: ModelData
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        modelData.landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
+                }
+                ForEach(filteredLandmarks) {landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+                    
+                }
+               
+            }
+            .navigationTitle(/*@START_MENU_TOKEN@*/"Landmarks"/*@END_MENU_TOKEN@*/)
+        }
+        
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+            .environmentObject(ModelData())
+        
+    }
+}
